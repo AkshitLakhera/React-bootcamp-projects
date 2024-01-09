42fa34ebaf52ab96e22b5239cba4461aa2369027
“[Akshit <akshitlakhera14@gmail.com>
2024-01-09 12:56:45 +0530

    Done with adding deleting functionality

--- React  project 3/travel-list/src/App.js
+++ React  project 3/travel-list/src/App.js
@@ -8,11 +8,14 @@ export default function  App(){
     function handleAddItems(item){
         setItems((items) => [...items,item]);
     }
+    function handleDeleteItems(id){
+        setItems((items) => items.filter((item) => item.id !== id));
+    }
 return(
     <div className='app'>
     <Logo/>
     <Form onAddItems={handleAddItems}/>
-    <PackingList items={items}/>
+    <PackingList items={items}  onDeleteItems={handleDeleteItems}/>
     <Stats/>
 
     </div>
@@ -53,23 +56,23 @@ return (
     </form>
 )
 }
-function PackingList ({items}){
+function PackingList ({items,onDeleteItems}){
 return(
 <div className = 'list'>
     <ul>
-    {items.map(item=>(<Item item = {item} key = {item.id}/>))}
+    {items.map(item=>(<Item item = {item}  key = {item.id}  onDeleteItems={onDeleteItems} />))}
 
 </ul>
 </div>
 );
 }
-function Item({item}){
+function Item({item , onDeleteItems }){
     return (
     <li>
         <span style={item.packed ? {textDecoration:'line-through' }:{}}>
             {item.quantity} {item.description}
         </span>
-        <button>❌</button>
+        <button onClick={() => onDeleteItems(item.id)}>❌</button>
         
     </li>
     );
