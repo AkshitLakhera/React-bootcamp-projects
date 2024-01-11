f8ee82c4013b42f60ec90895f9a2b2dc5840d283
“[Akshit <akshitlakhera14@gmail.com>
2024-01-11 17:01:54 +0530

    Added early return in footer

--- React  project 3/travel-list/src/App.js
+++ React  project 3/travel-list/src/App.js
@@ -85,12 +85,15 @@ function Item({item , onDeleteItems , onToggleItems}){
     );
 }
 function Stats ({items}){
+    if (!items.length) return( <p className='stats'>
+  <em> Start adding some items to your packing list</em>
+    </p>)
     const totalItems = items.length;
     const numPacked =  items.filter((item) => item.packed).length;
     const percentage = Math.round(numPacked/totalItems * 100)
     return(
     <footer className='stats'>
-        <em>You have {totalItems} items on your list, and you already packed {numPacked} & is percent  {percentage}% packed</em>
+    <em>You have {totalItems} items on your list, and you already packed {numPacked} & is percent  {percentage}% packed</em>
     </footer>
 )
 }
